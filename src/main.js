d4df9c17489da6f4ea804ed428d93933ae977234
Abigail Peña <97539104+PolarisSchulz@users.noreply.github.com>
2022-02-23 00:05:58 -0600

    chingos de cambios

--- src/main.js
+++ src/main.js
@@ -1,57 +1,17 @@
-//import { example } from "./data.js";
-import lolData from "./data/lol/lol.js";
+// estas funciones son de ejemplo
 
-//------------ Definición de variables, chí cheñol ------------//
+//export const example = () => {
+// return "example";
+//};
 
-// Variables para mosaico //
-let championData = lolData.data;
-let championCards = document.getElementById("champion-cards");
-
-// Variables para filtros //
-
-//------------ Función para mosaico de campeones ------------//
-function buildCards() {
-  let output = "";
-
-  //function buildCarrousel() {
-  //Integrar función para carrousel de tarjetas de campeón }
-
-  for (let champion in championData) {
-    if (championData.hasOwnProperty(champion)) {
-      let championObjectData = championData[champion];
-      console.log(championObjectData);
-      let {
-        blurb,
-        id,
-        image,
-        img,
-        info,
-        key,
-        name,
-        partype,
-        splash,
-        stats,
-        tags,
-        title,
-        version,
-      } = championObjectData;
-
-      output += `
-            <div class="${id}-class" champion-card >
-                <img  class="champion-image"  src="${splash}" alt="${id}" onclick="goToChampion(${id})">
-            </div>
-            `;
+export function filterFunction(allChampions, championClass) {
+  let arrayOfChampionsWithTags = [];
+  allChampions.forEach((champion) => {
+    let tags = champion.tags;
+    if (tags.includes(championClass)) {
+      arrayOfChampionsWithTags.push(champion);
     }
-  }
+  });
 
-  championCards.innerHTML += output;
+  return arrayOfChampionsWithTags;
 }
-
-//------------ Función para segunda vista (goToChampion) ------------//
-function goToChampion(id) {
-  // obtener el dato id para poder obtener el objeto de ese campeon a partir de champion data
-  // mandarnos a champion.html
-  // llenar champion.html con los datos que obtuvimos del campeón
-}
-
-buildCards();
