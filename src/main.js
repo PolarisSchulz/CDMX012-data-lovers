11fd659754d58feda7d3462394a8b2c935b8e59f
OromeSenryu <70779127+OromeSenryu@users.noreply.github.com>
2022-02-24 22:31:33 -0600

    Avances grandotes en los filtros de dropdown

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //import { example } from "./data.js";
 import lolData from "./data/lol/lol.js";
-import { filterFunction } from "./data.js";
+import { filterFunction, lowDifficultyFunction, highToLowAttackFunction } from "./data.js";
 
 //------------ Definición de variables, chí cheñol ------------//
 
@@ -57,22 +57,54 @@ function buildCards(champions) {
 document.querySelectorAll(".champClass_button").forEach((button) => {
   button.addEventListener("click", (event) => {
     let championDataArray = Object.values(championData);
-
-    let finalArray = filterFunction(championDataArray, event.target.value);
-
-    // console.log("finalArray => ", finalArray);
-
-    buildCards(finalArray);
+    let finalClassArray = filterFunction(championDataArray, event.target.value);
+    buildCards(finalClassArray);
   });
 });
+
 filter_all.addEventListener("click", () => {
   buildCards(championData);
 });
+
+//------------ Función para filtros por dificultad ------------//
+const lowDifficulty = document.querySelector(".lowDifficulty");
+const moderateDifficulty = document.querySelector(".moderateDifficulty");
+const highDifficulty = document.querySelector(".highDifficulty");
+
+// Low Difficulty //
+lowDifficulty.addEventListener("click", (event) => {
+  let championDataArray = Object.values(championData);
+  const rangeDifficulty = 3;
+  const getLowDifficulty =  championDataArray.filter((e)=>( e.info.difficulty <= rangeDifficulty));
+  //console.log("Low difficulty champs from", getLowDifficulty);
+  let finalLowDiffArray = lowDifficultyFunction(championDataArray);
+  // console.log("Revisando array de dificultad baja desde", finalLowDiffArray);
+  buildCards(finalLowDiffArray);
+});
+
+// Moderate Difficulty //
+
+
+
+//------------ Función para filtros por ataque/defensa ------------//
+const highestAttack = document.querySelector(".highestAttack");
+const lowestAttack = document.querySelector(".lowestAttack");
+const highestDefense = document.querySelector(".highestDefense");
+const lowestDefense = document.querySelector(".lowestDefense");
+
+highestAttack.addEventListener("click", (event) => {
+  let championDataArray = Object.values(championData);
+  let finalHighAtkArray = highToLowAttackFunction(championDataArray);
+  console.log(finalHighAtkArray)
+
+
+});
+
 //------------ Función para segunda vista (goToChampion) ------------//
 //function goToChampion(id) {
 // obtener el dato id para poder obtener el objeto de ese campeon a partir de champion data
 // mandarnos a champion.html
 // llenar champion.html con los datos que obtuvimos del campeón
-//}
+// }
 
 buildCards(championData);
