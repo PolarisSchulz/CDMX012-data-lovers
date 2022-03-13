0e443a02934c335554a1e88f1ed9303b9637c150
OromeSenryu <70779127+OromeSenryu@users.noreply.github.com>
2022-03-13 17:40:28 -0600

    Estilizado de segunda vista :B  (#11)
    * CSS de segunda vista: Detalles de campeón
    
    * Se añaden más detalles de campeón
    
    * Se añaden más detalles de campeón
    
    * Pequeñas correcciones ortográficas
    
    Co-authored-by: Abigail Peña <97539104+PolarisSchulz@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -179,29 +179,39 @@ function showElement(element) {
 }
 
 function populateChampion(currentChampion) {
+  //First section
   let name = currentChampion.name;
   let title = currentChampion.title;
   let splash = currentChampion.splash;
   let blurb = currentChampion.blurb;
-
   let tags = currentChampion.tags;
   let difficulty = currentChampion.info.difficulty;
+  // Second senction: Stats
   let attack = currentChampion.info.attack;
   let defense = currentChampion.info.defense;
   let magic = currentChampion.info.magic;
+  // Third section: substats
+  let hp = currentChampion.stats.hp;
+  let mp = currentChampion.stats.mp;
+  let armor = currentChampion.stats.armor;
+  let spellblock = currentChampion.stats.spellblock;
 
+  // Stats to DOM section
   document.getElementById("champion-title").innerHTML = title;
   document.getElementById(
     "champion-face"
   ).style.backgroundImage = `url('${splash}')`;
   document.getElementById("champion-name").innerHTML = name;
   document.getElementById("champion-blurb").innerHTML = blurb;
-
   document.getElementById("champion-tags").innerHTML = tags;
   document.getElementById("champion-difficulty").innerHTML = difficulty;
   document.getElementById("champion-attack").innerHTML = attack;
   document.getElementById("champion-defense").innerHTML = defense;
   document.getElementById("champion-magic").innerHTML = magic;
+  document.getElementById("champion-hp").innerHTML = hp;
+  document.getElementById("champion-mp").innerHTML = mp;
+  document.getElementById("champion-armor").innerHTML = armor;
+  document.getElementById("champion-spellblock").innerHTML = spellblock;
 }
 
 const mainMenuButton = document.getElementById("mainMenuButton");
