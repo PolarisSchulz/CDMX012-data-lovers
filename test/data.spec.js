fc2ba957f33ea871563b512c3e6b623f8692182b
OromeSenryu <70779127+OromeSenryu@users.noreply.github.com>
2022-03-12 19:27:57 -0600

    Update data.spec.js

--- test/data.spec.js
+++ test/data.spec.js
@@ -163,13 +163,3 @@ describe("lowToHighDefenseFunction", () => {
     ]);
   });
 });
-
-// describe.skip("anotherExample", () => {
-//   it("is a function", () => {
-//     expect(typeof anotherExample).toBe("function");
-//   });
-
-//   it("returns `anotherExample`", () => {
-//     expect(anotherExample()).toBe("OMG");
-//   });
-// });
